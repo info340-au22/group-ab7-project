638f588cc038b7773ad865fef3f4985318f4fd56
BuLingReactor <bulingreactor@163.com>
2022-10-24 19:07:53 -0700

    demo page side-bar

---
+++ script/site-page.js
@@ -0,0 +1,47 @@
+'use strict';
+
+(function () {
+  window.addEventListener('load', init);
+
+  /**
+   * initlization of the program.
+   */
+  function init() {}
+
+  /**
+   * Returns the element that has the ID attribute with the specified value.
+   * @param {string} idName - element ID
+   * @returns {HTMLElement} DOM object associated with id.
+   */
+  function id(idName) {
+    return document.getElementById(idName);
+  }
+
+  /**
+   * Returns a new element with the given tag name.
+   * @param {string} tagName - HTML tag name for new DOM element.
+   * @returns {HTMLElement} New DOM object for given HTML tag.
+   */
+  function gen(tagName) {
+    return document.createElement(tagName);
+  }
+
+  /**
+   * Returns an  elements matching the given query.
+   * @param {string} query - CSS query selector.
+   * @returns {HTMLElement} - the DOM objects matching the given query.
+   */
+  function qs(query) {
+    return document.querySelector(query);
+  }
+
+  /**
+   * Returns an array of elements matching the given query.
+   * @param {string} query - CSS query selector.
+   * @returns {array} - Array of DOM objects matching the given query.
+   */
+  function qsa(query) {
+    return document.querySelectorAll(query);
+  }
+
+})();
