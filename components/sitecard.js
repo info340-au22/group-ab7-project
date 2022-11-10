4eafce35b74550018a3cfad1c546f22802fccd80
tdao09 <tdao09@uw.edu>
2022-11-10 10:19:37 -0800

    starter code

---
+++ components/sitecard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import reactDOM from 'react-dom/client';
+
+
+function SiteCard(props) {
+    const state = props.state;
+    const descr = props.descr;
+    return(
+        // card syntax
+    );
+}
