c3ac4be9b3fd159124c2915e1fea76e3b5839dd5
BuLingReactor <bulingreactor@163.com>
2022-11-22 11:21:41 -0800

    footer classname and key fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,51 +1,78 @@
-import React from 'react';
+import React from "react";
 import footerData from "../data/authors.json";
 
 export default function Footer() {
-const footerDataArray = footerData;
+  const footerDataArray = footerData;
 
-let columnMap = footerDataArray.map((element) => {
+  let columnMap = footerDataArray.map((element, index) => {
     return (
-        <div className="containers">
+      <div className="containers" key={"container-" + index}>
         <div className="row">
-            <div className="col-4 padding-zero">
-            {element.columnName !== undefined ? <p className="bolded footer-header">{element.columnName}</p> : ""}
-            <p key={element.AuthorData} class="padding-zero pb-3">{element.AuthorData}</p>
-            </div>
+          <div className="col-4 padding-zero">
+            {element.columnName !== undefined ? (
+              <p className="bolded footer-header">{element.columnName}</p>
+            ) : (
+              ""
+            )}
+            <p key={element.AuthorData + index} className="padding-zero pb-3">
+              {element.AuthorData}
+            </p>
+          </div>
 
-            <div className="col-4 padding-zero">
-            {element.columnName2 !== undefined ? <p className="bolded footer-header">{element.columnName2}</p> : ""}
-            <p key={element.AuthorData} className="padding-zero pb-3"><a href={"mailto:" + element.AuthorData2}>{element.AuthorData2}</a></p>
-            </div>
+          <div className="col-4 padding-zero">
+            {element.columnName2 !== undefined ? (
+              <p className="bolded footer-header">{element.columnName2}</p>
+            ) : (
+              ""
+            )}
+            <p key={element.AuthorData + index} className="padding-zero pb-3">
+              <a href={"mailto:" + element.AuthorData2}>
+                {element.AuthorData2}
+              </a>
+            </p>
+          </div>
 
-            <div className="col-4 padding-zero">
-            {element.columnName3 !== undefined ? <p className="bolded footer-header">{element.columnName3}</p> : ""}
-            <p key={element.AuthorData} class="padding-zero pb-3"><a href={"tel:" + element.AuthorData3}>{element.AuthorData3}</a></p>
-            </div>
+          <div className="col-4 padding-zero">
+            {element.columnName3 !== undefined ? (
+              <p className="bolded footer-header">{element.columnName3}</p>
+            ) : (
+              ""
+            )}
+            <p key={element.AuthorData + index} className="padding-zero pb-3">
+              <a href={"tel:" + element.AuthorData3}>{element.AuthorData3}</a>
+            </p>
+          </div>
 
-            <form></form>
+          <form></form>
         </div>
-        </div>
+      </div>
     );
-});
+  });
 
-
-return(
+  return (
     <footer>
-       {columnMap}
+      {columnMap}
 
-        <p className="text-center m-0 pt-3">Subscribe to our Email!</p>
-        <div className="centering">
+      <p className="text-center m-0 pt-3">Subscribe to our Email!</p>
+      <div className="centering">
         <form>
-
-            <label className="p-3 " htmlFor="email_input ">Email:</label>
-            <input id="email_input " type="email " name="email
-            " placeholder="email@domain.com "/>
-            <button type="button ">Submit</button>
-
+          <label className="p-3 " htmlFor="email_input ">
+            Email:
+          </label>
+          <input
+            id="email_input "
+            type="email "
+            name="email
+            "
+            placeholder="email@domain.com "
+          />
+          <button type="button ">Submit</button>
         </form>
-        </div>
-        <p className="copyrighted centered "> &copy;2022 West Coast Tourist Recommendations</p>
-    </footer>);
+      </div>
+      <p className="copyrighted centered ">
+        {" "}
+        &copy;2022 West Coast Tourist Recommendations
+      </p>
+    </footer>
+  );
 }
-
