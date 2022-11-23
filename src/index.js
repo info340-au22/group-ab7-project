6a24d6c37920d6642af40d67bbbf904e742307a3
BuLingReactor <bulingreactor@163.com>
2022-11-23 08:17:45 -0800

    firebase configured

--- src/index.js
+++ src/index.js
@@ -3,7 +3,23 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCUEMG4dFxs5QVoEZN-6_uCvIZ4RaCOynA",
+  authDomain: "group-ab7-info340au22.firebaseapp.com",
+  projectId: "group-ab7-info340au22",
+  storageBucket: "group-ab7-info340au22.appspot.com",
+  messagingSenderId: "119434836765",
+  appId: "1:119434836765:web:cab133cc11cebe19e3bff4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
