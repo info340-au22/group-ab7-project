7ff0ceb5d29e1c2152d831fcfd462c3059f5b1c7
tdao09 <tdao09@uw.edu>
2022-12-11 23:16:45 -0800

    added a note for people not signed in

--- src/pages/SitesPage.js
+++ src/pages/SitesPage.js
@@ -6,6 +6,8 @@ import states from "../data/states.json";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { useSearchParams } from "react-router-dom";
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { getAuth } from "firebase/auth";
 
 const typeFilter = ["Cultural", "Natural"];
 
@@ -53,86 +55,182 @@ export default function SitesPage(props) {
     window.location.href =
       "/sites?search=" + document.getElementById("title-search").value;*/
   }
-  return (
-    <div>
-      <main>
-        <div className="content-area">
-          <section className="page-search">
-            <div className="page-search-bar">
-              <input
-                id="title-search"
-                type="text"
-                defaultValue={searchContent}
-                placeholder="Search.."
-                onKeyDown={(event) => {
-                  if (event.which === 13) {
-                    search();
-                  }
-                }}
-                onChange={(event) => {
-                  if (event.target.value === "") {
-                    search();
-                  }
-                }}
-              />
-              <button type="submit" onClick={search}>
-                <FontAwesomeIcon
-                  className="page-search-icon"
-                  icon={faMagnifyingGlass}
-                />
-              </button>
-            </div>
-          </section>
+  const auth = getAuth();
+  const [user, loading, error] = useAuthState(auth);
+  if (loading) {
+    return <p>Loading user data</p>
+  }
 
-          <section className="filter-box">
-            <h3>Filters:</h3>
-            <div className="filter">
-              <h4>State:</h4>
-              <ul>
-                {states.map((element) => (
-                  <li
-                    key={element.name}
-                    data-filter-name={element.short}
-                    className="uncard-selected"
-                    onClick={statesFilterClicked}
-                  >
-                    <div>
-                      <span>{element.name} </span>
-                      <span className="cancel not-shown">×</span>
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <div className="filter">
-              <h4>Type:</h4>
-              <ul>
-                {typeFilter.map((element) => (
-                  <li
-                    key={element}
-                    data-filter-name={element.toLowerCase()}
-                    className="uncard-selected"
-                    onClick={siteFilterClicked}
-                  >
-                    <div>
-                      <span>{element} </span>
-                      <span className="cancel not-shown">×</span>
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </section>
-            <p> Note: Bookmarking feature is for signed in users </p>
-            <SitesBox // SitesBox returns a <section>
-              filterStates={stateFilter}
-              filterType={siteTypeFilter}
-              titleSearch={titleSearchContent}
-              setState={props.setState}
-              state={props.state}
-            />
-        </div>
-      </main>
-    </div>
-  );
-}
+  if(error) {
+    return <p>There is an error: {error}</p>
+  }
+
+  if (user) {
+    return (
+      <div>
+        <main>
+          <div className="content-area">
+            <section className="page-search">
+              <div className="page-search-bar">
+                <input
+                  id="title-search"
+                  type="text"
+                  defaultValue={searchContent}
+                  placeholder="Search.."
+                  onKeyDown={(event) => {
+                    if (event.which === 13) {
+                      search();
+                    }
+                  }}
+                  onChange={(event) => {
+                    if (event.target.value === "") {
+                      search();
+                    }
+                  }}
+                />
+                <button type="submit" onClick={search}>
+                  <FontAwesomeIcon
+                    className="page-search-icon"
+                    icon={faMagnifyingGlass}
+                  />
+                </button>
+              </div>
+            </section>
+  
+            <section className="filter-box">
+              <h3>Filters:</h3>
+              <div className="filter">
+                <h4>State:</h4>
+                <ul>
+                  {states.map((element) => (
+                    <li
+                      key={element.name}
+                      data-filter-name={element.short}
+                      className="uncard-selected"
+                      onClick={statesFilterClicked}
+                    >
+                      <div>
+                        <span>{element.name} </span>
+                        <span className="cancel not-shown">×</span>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="filter">
+                <h4>Type:</h4>
+                <ul>
+                  {typeFilter.map((element) => (
+                    <li
+                      key={element}
+                      data-filter-name={element.toLowerCase()}
+                      className="uncard-selected"
+                      onClick={siteFilterClicked}
+                    >
+                      <div>
+                        <span>{element} </span>
+                        <span className="cancel not-shown">×</span>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </section>
+              <SitesBox // SitesBox returns a <section>
+                filterStates={stateFilter}
+                filterType={siteTypeFilter}
+                titleSearch={titleSearchContent}
+                setState={props.setState}
+                state={props.state}
+              />
+          </div>
+        </main>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <main>
+          <div className="content-area">
+            <section className="page-search">
+              <div className="page-search-bar">
+                <input
+                  id="title-search"
+                  type="text"
+                  defaultValue={searchContent}
+                  placeholder="Search.."
+                  onKeyDown={(event) => {
+                    if (event.which === 13) {
+                      search();
+                    }
+                  }}
+                  onChange={(event) => {
+                    if (event.target.value === "") {
+                      search();
+                    }
+                  }}
+                />
+                <button type="submit" onClick={search}>
+                  <FontAwesomeIcon
+                    className="page-search-icon"
+                    icon={faMagnifyingGlass}
+                  />
+                </button>
+              </div>
+            </section>
+  
+            <section className="filter-box">
+              <h3>Filters:</h3>
+              <div className="filter">
+                <h4>State:</h4>
+                <ul>
+                  {states.map((element) => (
+                    <li
+                      key={element.name}
+                      data-filter-name={element.short}
+                      className="uncard-selected"
+                      onClick={statesFilterClicked}
+                    >
+                      <div>
+                        <span>{element.name} </span>
+                        <span className="cancel not-shown">×</span>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="filter">
+                <h4>Type:</h4>
+                <ul>
+                  {typeFilter.map((element) => (
+                    <li
+                      key={element}
+                      data-filter-name={element.toLowerCase()}
+                      className="uncard-selected"
+                      onClick={siteFilterClicked}
+                    >
+                      <div>
+                        <span>{element} </span>
+                        <span className="cancel not-shown">×</span>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </section>
+              <p> Note: Bookmarking feature is for signed in users </p>
+              <SitesBox // SitesBox returns a <section>
+                filterStates={stateFilter}
+                filterType={siteTypeFilter}
+                titleSearch={titleSearchContent}
+                setState={props.setState}
+                state={props.state}
+              />
+          </div>
+        </main>
+      </div>
+    );
+  }
+  
+  }
+  
