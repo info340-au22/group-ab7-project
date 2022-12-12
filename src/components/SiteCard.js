1263942121808408ede5d149dc4bf32b63c47eac
tdao09 <tdao09@uw.edu>
2022-12-12 01:36:00 -0800

    progress

--- src/components/SiteCard.js
+++ src/components/SiteCard.js
@@ -38,6 +38,14 @@ export default function SiteCard(props) {
       const stateCopy = { ...props.state };
       const db = getDatabase();
       const bookmarkRef = ref(db, "sitesDetail/" + name + "/usersBookmarked/" + "/" + userID);
+       //if they don't have bookmark boolean data saved in the database
+      // specifically in sitesDetail/nameOfSite//userBookmarked/userID
+      // basically, if their userID is not in userBookmarked
+        // add it and set it to false
+      if(stateCopy[name].usersBookmarked[userID] === undefined) {
+        stateCopy[name].usersBookmarked[userID] = false;
+      }
+
       stateCopy[name].bookmarked = !stateCopy[name].bookmarked;
         if (stateCopy[name].usersBookmarked === undefined || stateCopy[name].usersBookmarked[userID] === false) {
           stateCopy[name].usersBookmarked[userID] = true;
@@ -47,11 +55,11 @@ export default function SiteCard(props) {
         props.setState(stateCopy);
         set(bookmarkRef, stateCopy[name].usersBookmarked[userID]);
     }
-
       if (
         //props.state === undefined ||
         //props.state[name] === undefined ||
         props.state[name].usersBookmarked === undefined ||
+        props.state[name].usersBookmarked[userID] === undefined ||
         props.state[name].usersBookmarked[userID] === false
       ) {
         imgSrc = "./img/bookmark.png";
